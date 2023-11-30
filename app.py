db981e91ccf92566627c18d741e3b217ea2b2742
Hasanah Nisa <64188572+hasanahnisa@users.noreply.github.com>
2023-11-30 22:02:24 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/produk')
+def produk():
+   return render_template('produk.html')
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
