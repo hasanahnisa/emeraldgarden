70f228600a42300a6ae3c9285d8e095db882d555
Naufal Rifqi Zuhrian <estarossa.nrz@gmail.com>
2023-12-25 19:08:34 +0800

    crud fasilitas

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, jsonify, request
 from pymongo import MongoClient
+from bson import ObjectId
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -24,6 +25,8 @@ TOKEN_KEY = 'my_token'
 def insert_admin():
    doc = {
             "username": "admin",                               
+            "name": "admin",                               
+            "photo": "admin.jpg",                               
             "password": "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918",                                                                                   
             }
    find_admin = db.admin.find_one({'username':'admin'})
@@ -52,10 +55,12 @@ def adminFasilitas():
                algorithms=['HS256']
             )
             user_info = db.admin.find_one({'username':payload['id']})
-         
+            fasilitas = db.fasilitas.find()
+            data_fasilitas = list(fasilitas)
+
             
             if user_info:
-               return render_template('adm-fasilitas.html',user_info=user_info)
+               return render_template('adm-fasilitas.html',user_info=user_info, data_fasilitas=data_fasilitas)
             else:
                return render_template('login.html')
                
@@ -68,6 +73,181 @@ def adminFasilitas():
    else:
       return render_template('login.html',msg="token tidak ada")
 
+@app.route('/tambah/fasilitas',  methods=['POST'])
+def tambahFasilitas():
+   msg = request.args.get('msg')
+   token_receive = request.cookies.get(TOKEN_KEY)
+   date = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+   if token_receive:
+      try:
+            payload = jwt.decode(
+               token_receive,
+               SECRET_KEY,
+               algorithms=['HS256']
+            )
+            user_info = db.admin.find_one({'username':payload['id']})
+         
+            
+            if user_info:
+               if request.method == 'POST':
+                  # Ambil data dari form
+                  nama = request.form['nama_fasilitas']
+                  jenis = request.form['jenis_fasilitas']
+                  deskripsi = request.form['deskripsi_fasilitas']
+                  
+                  doc = {
+                     "nama": nama,
+                     "jenis": jenis,
+                     "deskripsi": deskripsi,
+                  }
+                  
+                  if "gambar_fasilitas" in request.files and request.files["gambar_fasilitas"].filename:
+                     file = request.files["gambar_fasilitas"]
+                     filename = secure_filename(file.filename)
+                     extension = filename.split(".")[-1]
+                     file_path = f"gambar_fasilitas/{nama}_{date}.{extension}"
+                     file.save(os.path.join(os.getcwd(), 'static', file_path))
+                     doc["gambar"] = file_path
+                  else:
+                     doc["gambar"] = ''
+
+                  # Tambahkan data baru ke MongoDB
+                  db.fasilitas.insert_one(doc)
+
+                  return redirect(url_for('adminFasilitas'))
+               
+            else:
+               return render_template('login.html')
+               
+      except jwt.ExpiredSignatureError:
+            msg='Your token has expired'
+            return redirect(url_for('login', msg=msg))
+      except jwt.exceptions.DecodeError:
+            msg='There was a problem logging you in'
+            return redirect(url_for('login', msg=msg))
+   else:
+      return render_template('login.html',msg="token tidak ada")
+
+
+@app.route('/perbarui/fasilitas', methods=['POST'])
+def perbaruiFasilitas():
+   msg = request.args.get('msg')
+   token_receive = request.cookies.get(TOKEN_KEY)
+   date = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+   if token_receive:
+      try:
+            payload = jwt.decode(
+               token_receive,
+               SECRET_KEY,
+               algorithms=['HS256']
+            )
+            user_info = db.admin.find_one({'username':payload['id']})
+         
+            
+            if user_info:
+               if request.method == 'POST':
+                  id = request.form['id']
+                  nama = request.form['nama_fasilitas']
+                  jenis = request.form['jenis_fasilitas']
+                  deskripsi = request.form['deskripsi_fasilitas']
+
+                  # Ambil ID fasilitas dari parameter URL
+                  id_fasilitas = ObjectId(id)
+
+                  # Ambil dokumen fasilitas yang akan diperbarui
+                  fasilitas = db.fasilitas.find_one({'_id': id_fasilitas})
+                  
+                  if fasilitas:
+                        # Perbarui data pada dokumen fasilitas
+                        fasilitas["nama"] = nama
+                        fasilitas["jenis"] = jenis
+                        fasilitas["deskripsi"] = deskripsi
+                        gambar_lama = fasilitas.get('gambar', '')
+                        # Perbarui gambar jika ada file yang diunggah
+                        if "gambar_fasilitas" in request.files and request.files["gambar_fasilitas"].filename:
+                           file = request.files["gambar_fasilitas"]
+                           filename = secure_filename(file.filename)
+                           extension = filename.split(".")[-1]
+                           file_path = f"gambar_fasilitas/{nama}_{date}.{extension}"
+                           file.save(os.path.join(os.getcwd(), 'static', file_path))
+                           fasilitas["gambar"] = file_path
+                        else:
+                           fasilitas["gambar"] = ''  # Atau sesuaikan dengan logika yang Anda inginkan
+                           
+                        
+                        if gambar_lama:
+                           gambar_path = os.path.join(os.getcwd(), 'static', gambar_lama)
+                           try:
+                                 if os.path.exists(gambar_path):
+                                    os.remove(gambar_path)
+                           except Exception as e:
+                                 print(f"Error deleting old file: {e}")
+                        # Simpan perubahan ke MongoDB
+                        db.fasilitas.update_one({'_id': id_fasilitas}, {'$set': fasilitas})
+                        
+                        return redirect(url_for('adminFasilitas'))
+            else:
+               return render_template('login.html')
+               
+      except jwt.ExpiredSignatureError:
+            msg='Your token has expired'
+            return redirect(url_for('login', msg=msg))
+      except jwt.exceptions.DecodeError:
+            msg='There was a problem logging you in'
+            return redirect(url_for('login', msg=msg))
+   else:
+      return render_template('login.html',msg="token tidak ada")   
+   
+@app.route('/hapus/fasilitas/<id_fasilitas>')
+def hapusFasilitas(id_fasilitas):
+   msg = request.args.get('msg')
+   token_receive = request.cookies.get(TOKEN_KEY)
+
+   if token_receive:
+      try:
+            payload = jwt.decode(
+               token_receive,
+               SECRET_KEY,
+               algorithms=['HS256']
+            )
+            user_info = db.admin.find_one({'username':payload['id']})
+            fasilitas = db.fasilitas.find_one({'_id':ObjectId(id_fasilitas)})
+            
+            if fasilitas:
+               if user_info:
+                  if 'gambar' in fasilitas:
+                     gambar_path = os.path.join(os.getcwd(), 'static', fasilitas['gambar'])
+                     if os.path.exists(gambar_path):
+                        os.remove(gambar_path)
+                     db.fasilitas.delete_one({'_id':ObjectId(id_fasilitas)})
+                     return redirect(url_for('adminFasilitas'))
+               else:
+                  return render_template('login.html')
+               
+      except jwt.ExpiredSignatureError:
+            msg='Your token has expired'
+            return redirect(url_for('login', msg=msg))
+      except jwt.exceptions.DecodeError:
+            msg='There was a problem logging you in'
+            return redirect(url_for('login', msg=msg))
+   else:
+      return render_template('login.html',msg="token tidak ada")   
+
+@app.route('/get_fasilitas/<id_fasilitas>', methods=['GET'])
+def get_fasilitas(id_fasilitas):
+   fasilitas = db.fasilitas.find_one({'_id': ObjectId(id_fasilitas)})
+   print(fasilitas)
+   
+   if fasilitas:
+      return jsonify({
+         '_id': str(fasilitas['_id']),
+         'nama': fasilitas['nama'],
+         'jenis': fasilitas['jenis'],
+         'deskripsi': fasilitas['deskripsi'],
+         'gambar': fasilitas['gambar'] if 'gambar' in fasilitas else None
+      })
+   else:
+      return jsonify({'error': 'Fasilitas not found'}), 404
 
 @app.route('/admin/produk')
 def adminProduk():
